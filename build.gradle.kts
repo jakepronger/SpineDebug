2bc6dce5128f55d5b0fdb0ddafa9331eafef6ece
Jake Pronger <jakepronger31@gmail.com>
2025-12-17 20:12:21 +1100

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
 
     // Example of pulling a Git repo via JitPack (replace USER/REPO/BRANCH)
-    implementation("com.github.jakepronger:Spine:1.0.4")
+    implementation("com.github.jakepronger:Spine:1.0.5")
 
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
--- src/main/java/me/jakepronger/spinedebug/SpineDebug.java
+++ src/main/java/me/jakepronger/spinedebug/SpineDebug.java
@@ -4,11 +4,10 @@ import me.jakepronger.spine.Spine;
 import me.jakepronger.spine.interfaces.Dispatcher;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SpineDebug extends JavaPlugin {
 
     private SpineDebug plugin;
+
     private Spine spine;
     private Dispatcher dispatcher;
 
@@ -16,6 +15,7 @@ public class SpineDebug extends JavaPlugin {
     public void onEnable() {
         this.plugin = this;
         this.spine = new Spine(plugin);
+        this.dispatcher = spine.getDispatcher();
     }
 
     @Override
